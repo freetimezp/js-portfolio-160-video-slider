c119298ee7a512990079334fd758a1d6ba107823
freetimezp <kraftukrnet@ukr.net>
2024-11-23 23:16:58 +0200

    02. js-portfolio-160-video-slider. Main UI.

---
+++ src/components/slider/videos.js
@@ -0,0 +1,30 @@
+export const videos = [
+    {
+        id: "711863471",
+        title: "Cyberprint",
+        category: "Sci-Fi",
+        date: "May 2022",
+        url: "https://cdn.pixabay.com/video/2021/10/10/91562-629172467_large.mp4"
+    },
+    {
+        id: "478246234",
+        title: "Cyberprint",
+        category: "Documentary",
+        date: "Autumn 2022",
+        url: "https://cdn.pixabay.com/video/2019/05/22/23881-337972830_tiny.mp4"
+    },
+    {
+        id: "387407107",
+        title: "Cyberprint",
+        category: "Nature",
+        date: "October 2023",
+        url: "https://cdn.pixabay.com/video/2023/10/15/185092-874643408_tiny.mp4"
+    },
+    {
+        id: "704562417",
+        title: "Cyberprint",
+        category: "Art",
+        date: "January 2021",
+        url: "https://cdn.pixabay.com/video/2024/06/06/215500_tiny.mp4"
+    },
+];
